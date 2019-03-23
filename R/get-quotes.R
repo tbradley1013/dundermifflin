0b63d9af609107274346504ad165a35d229de68c
Tyler Bradley <tyler.bradley@phila.gov>
2019-03-23 18:25:53 -0400

    added main character default for get_quote

--- R/get-quotes.R
+++ R/get-quotes.R
@@ -2,24 +2,81 @@
 #'
 #' Randomly pull quotes from the office_quotes data frame
 #'
+#' @param ... arguments to pass to \link[dundermifflin]{filter_quotes}
+#' @param output a character speicfying whether you want a character string
+#' or a tibble output of the quote information
+#'
+#' @export
+get_quote <- function(..., output = c("character", "tbl")){
+  quotes <- filter_quotes(...)
+
+  row_n <- nrow(quotes)
+
+  row_pick <- sample(1:row_n, 1)
+
+  quote <- quotes[row_pick, ]
+  # return(quote)
+
+  output <- match.arg(output, choices = c("character", "tbl"))
+
+
+  cat(
+    quote$quote,
+    "\n~ ",
+    quote$character,
+    "\n",
+    "Season ", quote$season,
+    ", Epsiode ", quote$episode, " - ",
+    quote$name,
+    "\nword count = ", quote$nwords,
+    sep = ""
+  )
+
+  if (output == "character"){
+    out <- paste(
+      quote$quote,
+      "\n~ ",
+      quote$character,
+      "\n",
+      "Season ", quote$season,
+      ", Epsiode ", quote$episode, " - ",
+      quote$name,
+      sep = ""
+    )
+    invisible(out)
+  } else {
+    invisible(quote)
+  }
+
+
+}
+
+
+
+
+#' Filter office_quotes
+#'
 #' @param season an integer specifyng the season for which to get quotes from
 #' Currently, all seasons are included except season 5
 #' @param episode an integer specifying the episode number to get quote from
 #' @param name a character string with the episode name to get quote from
 #' @param scene an integer specifying the scene number
-#' @param character a character specifying the character to get quote from
-#' @param output a character speicfying whether you want a character string
-#' or a tibble output of the quote information
+#' @param character a character specifying the character to get quote from.
+#' Defaults to 'main', see details
 #' @param min_nword the minimum number of words a quote must have to be
 #' included, defaults to 4
 #' @param max_nword the maximum number of words a quote can have to be included,
 #' defaults to 20
 #'
+#' @details
+#' Main Character List - Michael, Dwight, Jim, Pam, Andy, Kevin, Angela,
+#' Erin, Oscar, Ryan, Darryl, Phyllis, Toby, Kelly, Stanley, Meredith,
+#' and Creed
+#'
 #' @export
-get_quote <- function(season = NULL, episode = NULL, name = NULL,
-                      scene = NULL, character = NULL,
-                      output = c("character", "tbl"),
-                      min_nword = 4, max_nword = 20){
+filter_quotes <- function(season = NULL, episode = NULL, name = NULL,
+                          scene = NULL, character = "main",
+                          min_nword = 4, max_nword = 20){
   quotes <- dundermifflin::office_quotes
 
   if (!is.null(season)){
@@ -60,7 +117,15 @@ get_quote <- function(season = NULL, episode = NULL, name = NULL,
   }
 
   if (!is.null(character)){
-    quotes <- quotes[tolower(quotes$character) == tolower(character),]
+    if (character == "main"){
+      character <- c("Michael", "Dwight", "Jim", "Pam", "Andy", "Kevin",
+                     "Angela", "Erin", "Oscar", "Ryan", "Darryl",
+                     "Phyllis", "Toby", "Kelly", "Stanley",
+                     "Meredith", "Creed")
+      quotes <- quotes[tolower(quotes$character) %in% tolower(character),]
+    } else {
+      quotes <- quotes[tolower(quotes$character) == tolower(character),]
+    }
   }
 
   quotes$nwords <- stringi::stri_count(trimws(quotes$quote), regex = "\\s") + 1
@@ -73,44 +138,5 @@ get_quote <- function(season = NULL, episode = NULL, name = NULL,
   if (!is.null(max_nword)){
     quotes <- quotes[quotes$nwords <= max_nword,]
   }
-
-  row_n <- nrow(quotes)
-
-  row_pick <- sample(1:row_n, 1)
-
-  quote <- quotes[row_pick, ]
-  # return(quote)
-
-  output <- match.arg(output, choices = c("character", "tbl"))
-
-
-  cat(
-    quote$quote,
-    "\n~ ",
-    quote$character,
-    "\n",
-    "Season ", quote$season,
-    ", Epsiode ", quote$episode, " - ",
-    quote$name,
-    "\nword count = ", quote$nwords,
-    sep = ""
-  )
-
-  if (output == "character"){
-    out <- paste(
-      quote$quote,
-      "\n~ ",
-      quote$character,
-      "\n",
-      "Season ", quote$season,
-      ", Epsiode ", quote$episode, " - ",
-      quote$name,
-      sep = ""
-    )
-    invisible(out)
-  } else {
-    invisible(quote)
-  }
-
 
 }
