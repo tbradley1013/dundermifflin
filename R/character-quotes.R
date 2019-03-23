bdcb77209f7e8c2ce66c0a0ca45d5ca0020121ca
Tyler Bradley <tyler.bradley@phila.gov>
2019-03-23 18:56:09 -0400

    added ... to character functions

--- R/character-quotes.R
+++ R/character-quotes.R
@@ -22,90 +22,90 @@ jim <- function(...){
 #' @rdname character_quotes
 #' @export
 dwight <- function(...){
-  get_quote(character = "Dwight")
+  get_quote(character = "Dwight", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 pam <- function(...){
-  get_quote(character = "Pam")
+  get_quote(character = "Pam", ...)
 }
 
 
 #' @rdname character_quotes
 #' @export
 andy <- function(...){
-  get_quote(character = "Andy")
+  get_quote(character = "Andy", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 kevin <- function(...){
-  get_quote(character = "Kevin")
+  get_quote(character = "Kevin", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 angela <- function(...){
-  get_quote(character = "Angela")
+  get_quote(character = "Angela", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 erin <- function(...){
-  get_quote(character = "Erin")
+  get_quote(character = "Erin", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 oscar <- function(...){
-  get_quote(character = "Oscar")
+  get_quote(character = "Oscar", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 ryan <- function(...){
-  get_quote(character = "ryan")
+  get_quote(character = "Ryan", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 darryl <- function(...){
-  get_quote(character = "Darryl")
+  get_quote(character = "Darryl", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 phyllis <- function(...){
-  get_quote(character = "Phyllis")
+  get_quote(character = "Phyllis", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 toby <- function(...){
-  get_quote(character = "Toby")
+  get_quote(character = "Toby", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 kelly <- function(...){
-  get_quote(character = "Kelly")
+  get_quote(character = "Kelly", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 stanley <- function(...){
-  get_quote(character = "Stanley")
+  get_quote(character = "Stanley", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 meredith <- function(...){
-  get_quote(character = "Meredith")
+  get_quote(character = "Meredith", ...)
 }
 
 #' @rdname character_quotes
 #' @export
 creed <- function(...){
-  get_quote(character = "Creed")
+  get_quote(character = "Creed", ...)
 }
