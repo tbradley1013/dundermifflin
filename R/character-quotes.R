d34e666c33ca9e223995f038dcea1f219fee2360
Tyler Bradley <tyler.bradley@phila.gov>
2019-03-23 18:26:13 -0400

    started creating functions for individual character quotes

---
+++ R/character-quotes.R
@@ -0,0 +1,51 @@
+#' Get quotes from specific characters
+#'
+#' @param ... arguments to be passed to \link[dundermifflin]{get_quote}
+#' @export
+#' @name character_quotes
+NULL
+
+#' @rdname character_quotes
+#' @export
+michael <- function(...){
+  get_quote(character = "Michael", ...)
+}
+
+
+#' @rdname character_quotes
+#' @export
+jim <- function(...){
+  get_quote(character = "Jim", ...)
+}
+
+
+#' @rdname character_quotes
+#' @export
+dwight <- function(...){
+  get_quote(character = "Dwight")
+}
+
+#' @rdname character_quotes
+#' @export
+pam <- function(...){
+  get_quote(character = "Pam")
+}
+
+
+#' @rdname character_quotes
+#' @export
+andy <- function(...){
+  get_quote(character = "Andy")
+}
+
+#' @rdname character_quotes
+#' @export
+kevin <- function(...){
+  get_quote(character = "Kevin")
+}
+
+#' @rdname character_quotes
+#' @export
+angela <- function(...){
+  get_quote(character = "Angela")
+}
