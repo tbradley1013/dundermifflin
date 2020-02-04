00764a75a488e0b750be0ccc6c4e6c930483889f
Tyler Bradley <tyler.bradley@phila.gov>
2020-02-04 09:31:55 -0500

    creating a get scene function

---
+++ R/get-scene.R
@@ -0,0 +1,57 @@
+# Get Scene
+
+
+random_scene <- function(){
+  season <- sample(c(1:4, 6:9), 1)
+  episode <- sample(ep_n_scene$episode[ep_n_scene$season == season], 1)
+  scene <- sample(ep_n_scene$scene[ep_n_scene$season == season & ep_n_scene$episode == episode], 1)
+
+  scene_quotes <- office_quotes[office_quotes$season == season & office_quotes$episode == episode & office_quotes$scene == scene,]
+  class(scene_quotes) <- c("dunder_scene", class(scene_quotes))
+  return(scene_quotes)
+}
+
+
+print.dunder_scene <- function(x, ...){
+  screen_width <- options()$width
+  max_char_length <- max(nchar(x$character))
+
+  # if (max_quote_length > screen_width){
+  #   width <- screen_width
+  # } else {
+  #   width <- max_quote_length
+  # }
+
+  if (length(unique(x$character)) <= 7){
+    cols <- c("red", "green", "yellow", "blue", "magenta", "cyan", "silver")
+    cols <- sample(cols, length(unique(x$character)), replace = FALSE)
+    names(cols) <- unique(x$character)
+  } else {
+    cols <- c("red", "green", "yellow", "blue", "magenta", "cyan", "silver")
+    cols <- sample(cols, length(unique(x$character)), replace = TRUE)
+    names(cols) <- unique(x$character)
+  }
+
+  header <- paste0("THE OFFICE - ", unique(x$name), " (Season ", unique(x$season), ", Episode ", unique(x$episode), ", Scene ", unique(x$scene), ")\n")
+  header_len <- nchar(header)
+  cat(rep("=", min(c(header_len, screen_width))), "\n", sep = "")
+  cat("THE OFFICE - ", crayon::red(unique(x$name)), " (Season ", unique(x$season), ", Episode ", unique(x$episode), ", Scene ", unique(x$scene), ")\n", sep = "")
+  cat(rep("=", min(c(header_len, screen_width))), "\n", sep = "")
+  for (i in 1:nrow(x)){
+    qt <- x[i,]
+    cat(
+      char_color(qt$character, cols),
+      ": ", rep(" ", (max_char_length - nchar(qt$character))),
+      qt$quote,
+      # "\n", rep(" ", (width - nchar(qt$character) - 3)), "~ ",
+      "\n",
+      sep = ""
+    )
+  }
+}
+
+
+char_color <- function(character, cols){
+  color <- cols[names(cols) == character]
+  crayon::style(character, crayon::make_style(color))
+}
